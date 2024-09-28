38f5dd8103851781474d116a29cc94ad2cd48d34
80808080 <138010338+s80808080@users.noreply.github.com>
2024-09-28 18:12:39 +0800

    Update config.mjs

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "nicetravel server",
   description: "Welcome everyone!",
+  base: '/repo/'
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
